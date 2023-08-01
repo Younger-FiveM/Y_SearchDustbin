da179addf8c7604a5604f9c1f9bc71b41b005ee1
SohanGnd <Younger_Developpment@hotmail.com>
2023-08-01 15:12:40 +0200

    init

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+--shared_script '@WaveShield/resource/waveshield.lua' --this line was automatically written by WaveShield
+
+version '1.0.0'
+author 'YOUNGER'
+description 'Y_AnchorBoat'
+
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+    'config.lua',
+    'client/main.lua',
+}
