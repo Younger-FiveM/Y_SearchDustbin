d3ed104eac81ddb00a28a63ea4425e1615438f88
SohanGnd <Younger_Developpment@hotmail.com>
2023-08-02 19:37:46 +0200

    Add function CansSearchDustbin
    The function is used to check whether the player can search a bi

--- client/main.lua
+++ client/main.lua
@@ -1,4 +1,4 @@
---------------------------------------------
+------------------------------------------------------------
 local ESX = nil
 local PlayerData = nil
 if Config.OldESX then
@@ -14,4 +14,31 @@ else
 	ESX = exports['es_extended']:getSharedObject()
 end
 
---------------------------------------------
+------------------------------------------------------------
+--------------------------FONCTION--------------------------
+local isSearching = false
+local blocked = false
+
+function CanSearchDustbin()
+    local playerPed = GetPlayerPed(-1)
+    local playerCoords = GetEntityCoords(playerPed)
+    local props = {}
+
+    for _, model in ipairs(Config.AllowedDustbinProps) do
+        local dustbin = GetClosestObjectOfType(playerCoords.x, playerCoords.y, playerCoords.z, 1.0, GetHashKey(model), false, false, false)
+        if DoesEntityExist(dustbin) then
+            table.insert(props, dustbin)
+        end
+    end
+
+    for i = 1, #props do
+        local dustbin = props[i]
+        local dusbinCoords = GetEntityCoords(dustbin)
+        local distance = #(playerCoords - dusbinCoords)
+
+        if not isSearching and distance <= Config.Range and not IsPedInAnyVehicle(playerPed, false) and not blocked[dustbin] then
+            return true
+        end
+    end
+    return false
+end
